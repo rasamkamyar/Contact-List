44a89bd1b8d0b01984360c080deba7be8c95ab76
rasamkamyar <rasamkamyar@gmail.com>
2024-08-12 17:36:17 +0330

    add form

--- src/Form/FormList.jsx
+++ src/Form/FormList.jsx
@@ -1,34 +1,40 @@
-import { Form, Input, Row, Col, Button } from "antd";
+import { Form, Input, Row, Col, Button, Flex } from "antd";
 import React from "react";
 
 function FormList() {
   return (
-    <Form labelCol={{ span: 6 }} wrapperCol={{ span: 12 }}>
-      <Row>
-        <Col span={12}>
-          <Form.Item>
-            <Input placeholder="Name" />
-          </Form.Item>
-        </Col>
-        <Col span={12}>
-          <Form.Item>
-            <Input placeholder="Last Name" />
-          </Form.Item>
-        </Col>
-      </Row>
-      <Row>
-        <Col span={12}>
-          <Form.Item>
-            <Input placeholder="Gmail" />
-          </Form.Item>
-        </Col>
-        <Col span={12}>
-          <Form.Item>
-            <Input placeholder="Number" />
-          </Form.Item>
-        </Col>
-      </Row>
-    </Form>
+    <Flex justify="center" align="center">
+      <Form
+        labelCol={{ span: 0 }}
+        wrapperCol={{ span: 24 }}
+        style={{ margin: "20px" }}
+      >
+        <Row gutter={24}>
+          <Col span={12}>
+            <Form.Item>
+              <Input placeholder="Name" size="large" />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item>
+              <Input placeholder="Last Name" size="large" />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row gutter={24}>
+          <Col span={12}>
+            <Form.Item>
+              <Input placeholder="Gmail" size="large" />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item>
+              <Input placeholder="Number" size="large" />
+            </Form.Item>
+          </Col>
+        </Row>
+      </Form>
+    </Flex>
   );
 }
 
@@ -40,61 +46,67 @@ export default FormList;
 //   };
 
 //   return (
-//     <Form
-//       name="custom_form"
-//       onFinish={onFinish}
-//       labelCol={{ span: 6 }}
-//       wrapperCol={{ span: 12 }}
+//     <div
+//       style={{
+//         display: "flex",
+//         alignItems: "center",
+//         justifyContent: "center",
+//       }}
 //     >
-//       <Row gutter={16}>
-//         <Col span={12}>
-//           <Form.Item
-//             name="name"
-//             label="Name"
-//             rules={[{ required: true, message: "Please enter your name" }]}
-//           >
-//             <Input placeholder="Enter your name" />
-//           </Form.Item>
-//         </Col>
-//         <Col span={12}>
-//           <Form.Item
-//             name="lastname"
-//             label="Last Name"
-//             rules={[{ required: true, message: "Please enter your last name" }]}
-//           >
-//             <Input placeholder="Enter your last name" />
-//           </Form.Item>
-//         </Col>
-//       </Row>
+//       <Form
+//         name="custom_form"
+//         onFinish={onFinish}
+//         labelCol={{ span: 2 }}
+//         wrapperCol={{ span: 22 }}
+//       >
+//         <Row gutter={16}>
+//           <Col span={18}>
+//             <Form.Item
+//               name="name"
+//               rules={[{ required: true, message: "Please enter your name" }]}
+//             >
+//               <Input placeholder="Enter your name" />
+//             </Form.Item>
+//           </Col>
+//           <Col span={12}>
+//             <Form.Item
+//               name="lastname"
+//               rules={[
+//                 { required: true, message: "Please enter your last name" },
+//               ]}
+//             >
+//               <Input placeholder="Enter your last name" />
+//             </Form.Item>
+//           </Col>
+//         </Row>
 
-//       <Row gutter={16}>
-//         <Col span={12}>
-//           <Form.Item
-//             name="gmail"
-//             label="Gmail"
-//             rules={[{ required: true, message: "Please enter your Gmail" }]}
-//           >
-//             <Input placeholder="Enter your Gmail" />
-//           </Form.Item>
-//         </Col>
-//         <Col span={12}>
-//           <Form.Item
-//             name="number"
-//             label="Number"
-//             rules={[{ required: true, message: "Please enter your number" }]}
-//           >
-//             <Input placeholder="Enter your number" />
-//           </Form.Item>
-//         </Col>
-//       </Row>
+//         <Row gutter={16}>
+//           <Col span={12}>
+//             <Form.Item
+//               name="gmail"
+//               rules={[{ required: true, message: "Please enter your Gmail" }]}
+//             >
+//               <Input placeholder="Enter your Gmail" />
+//             </Form.Item>
+//           </Col>
+//           <Col span={12}>
+//             <Form.Item
+//               name="number"
+//               rules={[{ required: true, message: "Please enter your number" }]}
+//             >
+//               <Input placeholder="Enter your number" />
+//             </Form.Item>
+//           </Col>
+//         </Row>
 
-//       <Form.Item wrapperCol={{ span: 12, offset: 6 }}>
-//         <Button type="primary" htmlType="submit">
-//           Submit
-//         </Button>
-//       </Form.Item>
-//     </Form>
+//         <Form.Item wrapperCol={{ span: 12, offset: 6 }}>
+//           <Button type="primary" htmlType="submit">
+//             Submit
+//           </Button>
+//         </Form.Item>
+//       </Form>
+//     </div>
 //   );
 // };
 
-//  export default CustomForm;
+// export default CustomForm;
